a8da7f737109ed8f0d2af9c2bca3fa8c86506fe3
DerZyklop <mail@der-zyklop.de>
2016-03-25 20:21:06 +0100

    added styles for advanced-search-controls

--- site/snippets/advanced-search-controls.php
+++ site/snippets/advanced-search-controls.php
@@ -1,55 +1,57 @@
-<section>
-  <h4>Branche</h4>
-  <ul>
-    <li><input type="checkbox"><a href="#">Banken</a></li>
-    <li><input type="checkbox"><a href="#">Handwerk</a></li>
-    <li><input type="checkbox"><a href="#">Dienstleistung</a></li>
-    <li><input type="checkbox"><a href="#">Kultur</a></li>
-    <li><input type="checkbox"><a href="#">Energie</a></li>
-    <li><input type="checkbox"><a href="#">Einzelhandel</a></li>
-    <li><input type="checkbox"><a href="#">Wohnung</a></li>
-    <li><input type="checkbox"><a href="#">Bildung</a></li>
-    <li><input type="checkbox"><a href="#">Landwirtschaft</a></li>
-    <li><input type="checkbox"><a href="#">Großhandel</a></li>
-    <li><input type="checkbox"><a href="#">Konsum</a></li>
-    <li><input type="checkbox"><a href="#">Soziales</a></li>
-  </ul>
-</section>
-<section>
-  <h4>Kategorie</h4>
-  <ul>
-    <li><input type="checkbox"><a href="#">Förderung</a></li>
-    <li><input type="checkbox"><a href="#">Öffentlich-Rechtliche</a></li>
-    <li><input type="checkbox"><a href="#">Dienstleistung</a></li>
-    <li><input type="checkbox"><a href="#">Produktion</a></li>
-    <li><input type="checkbox"><a href="#">Soziales und Kultur</a></li>
-    <li><input type="checkbox"><a href="#">Bürger</a></li>
-  </ul>
-</section>
-<section>
-  <h4>Ort</h4>
-  <ul>
-    <li><input type="checkbox"><a href="#">PLZ / Ort</a></li>
-  </ul>
-</section>
-<section>
-  <h4>Bundesland</h4>
-  <ul>
-    <li><input type="checkbox"><a href="#">Baden-Württemberg</a></li>
-    <li><input type="checkbox"><a href="#">Bayern</a></li>
-    <li><input type="checkbox"><a href="#">Berlin</a></li>
-    <li><input type="checkbox"><a href="#">Brandenburg</a></li>
-    <li><input type="checkbox"><a href="#">Bremen</a></li>
-    <li><input type="checkbox"><a href="#">Hamburg</a></li>
-    <li><input type="checkbox"><a href="#">Hessen</a></li>
-    <li><input type="checkbox"><a href="#">Mecklenburg-Vorpommern</a></li>
-    <li><input type="checkbox"><a href="#">Niedersachsen</a></li>
-    <li><input type="checkbox"><a href="#">Nordrhein-Westfalen</a></li>
-    <li><input type="checkbox"><a href="#">Rheinland-Pfalz</a></li>
-    <li><input type="checkbox"><a href="#">Saarland</a></li>
-    <li><input type="checkbox"><a href="#">Sachsen</a></li>
-    <li><input type="checkbox"><a href="#">Sachsen-Anhalt</a></li>
-    <li><input type="checkbox"><a href="#">Schleswig-Holstein</a></li>
-    <li><input type="checkbox"><a href="#">Thüringen</a></li>
-  </ul>
-</section>
+<div class="advanced-search-controls">
+  <section>
+    <h4>Branche</h4>
+    <ul>
+      <li><input type="checkbox">Banken</li>
+      <li><input type="checkbox">Handwerk</li>
+      <li><input type="checkbox">Dienstleistung</li>
+      <li><input type="checkbox">Kultur</li>
+      <li><input type="checkbox">Energie</li>
+      <li><input type="checkbox">Einzelhandel</li>
+      <li><input type="checkbox">Wohnung</li>
+      <li><input type="checkbox">Bildung</li>
+      <li><input type="checkbox">Landwirtschaft</li>
+      <li><input type="checkbox">Großhandel</li>
+      <li><input type="checkbox">Konsum</li>
+      <li><input type="checkbox">Soziales</li>
+    </ul>
+  </section>
+  <section>
+    <h4>Kategorie</h4>
+    <ul>
+      <li><input type="checkbox">Förderung</li>
+      <li><input type="checkbox">Öffentlich-Rechtliche</li>
+      <li><input type="checkbox">Dienstleistung</li>
+      <li><input type="checkbox">Produktion</li>
+      <li><input type="checkbox">Soziales und Kultur</li>
+      <li><input type="checkbox">Bürger</li>
+    </ul>
+  </section>
+  <section>
+    <h4>Ort</h4>
+    <ul>
+      <li><input type="checkbox">PLZ / Ort</li>
+    </ul>
+  </section>
+  <section>
+    <h4>Bundesland</h4>
+    <ul>
+      <li><input type="checkbox">Baden-Württemberg</li>
+      <li><input type="checkbox">Bayern</li>
+      <li><input type="checkbox">Berlin</li>
+      <li><input type="checkbox">Brandenburg</li>
+      <li><input type="checkbox">Bremen</li>
+      <li><input type="checkbox">Hamburg</li>
+      <li><input type="checkbox">Hessen</li>
+      <li><input type="checkbox">Mecklenburg-Vorpommern</li>
+      <li><input type="checkbox">Niedersachsen</li>
+      <li><input type="checkbox">Nordrhein-Westfalen</li>
+      <li><input type="checkbox">Rheinland-Pfalz</li>
+      <li><input type="checkbox">Saarland</li>
+      <li><input type="checkbox">Sachsen</li>
+      <li><input type="checkbox">Sachsen-Anhalt</li>
+      <li><input type="checkbox">Schleswig-Holstein</li>
+      <li><input type="checkbox">Thüringen</li>
+    </ul>
+  </section>
+</div>
