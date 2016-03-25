8ae8c4f5dbf9fc786e752cd72e53287726daad46
DerZyklop <mail@der-zyklop.de>
2016-03-25 19:39:32 +0100

    improved highlightedtext

---
+++ site/snippets/header.php
@@ -0,0 +1,48 @@
+  <header>
+    <a href="#" id="logo">
+      <img src="assets/images/logo.svg" width="230px" alt="">
+    </a>
+    <div id="search_controls_alternative"></div>
+    <nav class="mainnav">
+      <ul>
+        <li>
+          <a href="#">
+            Suche
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Landkarte
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Genossenschaften
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Mitglied werden
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Wir
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <nav class="usernav">
+      <ul>
+        <li>
+          <a href="#"><img class="icon" src="assets/images/google-location-icon-icon-location.png">Mitglieder</a>
+        </li>
+        <li>
+          <a href="#"><img class="icon" src="assets/images/google-location-icon-icon-location.png" />Merkliste</a>
+        </li>
+        <li>
+          <a href="#"><img class="icon" src="assets/images/google-location-icon-icon-location.png" />Kontakt</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
