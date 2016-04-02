043f2de0d09072c3de62e96b746ec025794ff694
DerZyklop <mail@der-zyklop.de>
2016-04-02 18:27:11 +0200

    improved results & searchparams

--- site/snippets/searchparams.php
+++ site/snippets/searchparams.php
@@ -1,17 +1,17 @@
 <aside id="searchparams">
   <ul class="selectors">
     <li>
-      <h5 class="title"><a href="#">- Trollololo</a></h5>
+      <h5 class="title"><a href="#"><img src="assets/images/google-location-icon-icon-location.png" height="14px"> Trollololo</a></h5>
       <ul>
         <li>Trolo</li>
         <li>lololo</li>
       </ul>
     </li>
     <li>
-      <h5 class="title"><a href="#">+ Trollololo</a></h5>
+      <h5 class="title"><a href="#"><img src="assets/images/google-location-icon-icon-location.png" height="14px"> Trollololo</a></h5>
     </li>
     <li>
-      <h5 class="title"><a href="#">- Trollololo</a></h5>
+      <h5 class="title"><a href="#"><img src="assets/images/google-location-icon-icon-location.png" height="14px"> Trollololo</a></h5>
       <ul>
         <li>Piu</li>
         <li>Piu</li>
