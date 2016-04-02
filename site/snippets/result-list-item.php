b4a986cacc3df92018d966ec2e1a9c3def9309cb
DerZyklop <mail@der-zyklop.de>
2016-04-02 18:26:31 +0200

    fixed header added result-list-item

---
+++ site/snippets/result-list-item.php
@@ -0,0 +1,10 @@
+<li>
+  <div class="eg_name">Muran Mainzer eG</div>
+  <ul class="eg_location">
+    <li><img src="assets/images/google-location-icon-icon-location.png" height="14px"> Mainzi</li>
+    <li><img src="assets/images/google-location-icon-icon-location.png" height="14px"> Mainzi</li>
+    <li><img src="assets/images/google-location-icon-icon-location.png" height="14px"> Mainzi</li>
+    <li><img src="assets/images/google-location-icon-icon-location.png" height="14px"> Mainzi</li>
+  </ul>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem iusto explicabo dolor dicta. Quisquam repellat sint sunt laudantium quasi cumque fuga omnis, ducimus nesciunt debitis. Ad est, ullam porro dolorum!</p>
+</li>
