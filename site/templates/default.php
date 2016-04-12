37622cc107833adbf6350b93cfd01c4a0d1b1c74
DerZyklop <mail@der-zyklop.de>
2016-04-12 11:17:44 +0200

    fixed default page

--- site/templates/default.php
+++ site/templates/default.php
@@ -9,31 +9,11 @@
 </head>
 <body>
   <div class="decoline"></div>
-  <section class="header">
-
-    <header>
-
-      <div class="two-col">
-        <div class="col-1">
-          <a href="/" id="logo">
-            <img src="/assets/images/logo.svg" width="230px" alt="">
-          </a>
-        </div>
-        <div class="col-2">
-          <?php snippet('usernav') ?>
-        </div>
-      </div>
-  <!--     <div id="search_controls_alternative"></div> -->
-      <div class="two-col">
-        <div class="col-1"></div>
-        <div class="col-2"><?php snippet('mainnav') ?></div>
-      </div>
-
-    </header>
-  </section>
+  <?php snippet('header') ?>
 
   <main class="page_wrap">
-    <?php echo $page->text() ?>
+    <h2><?= $page->title() ?></h2>
+    <p><?= $page->text() ?></p>
   </main>
   <footer class="page_wrap">
     <ul>
