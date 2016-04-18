8ceec68c1fa45b26ef862caea43e327e1cbe64e2
DerZyklop <mail@der-zyklop.de>
2016-04-18 17:39:59 +0200

    fixed metadata foreach

--- site/snippets/result-list-item.php
+++ site/snippets/result-list-item.php
@@ -1,9 +1,17 @@
 <li>
-  <div class="eg_name"><a href="<?= $company->url() ?>"><?= $company->title() ?></a></div>
+  <h3 class="eg_name"><a href="<?= $company->url() ?>"><?= $company->title() ?></a></h3>
   <ul class="eg_location">
-    <?php if ($company->city()): ?>
-      <li><img src="/assets/images/google-location-icon-icon-location.png" height="14px"> <?= $company->city() ?></li>
-    <?php endif ?>
+    <?php $meta_items = [
+      $company->city(),
+      $company->verband(),
+      $company->kategorie(),
+      $company->banche()
+    ]; ?>
+    <?php foreach ($meta_items as $item) : ?>
+      <?php if ($item && (String)$item->html() !== ""): ?>
+        <li><img src="/assets/images/google-location-icon-icon-location.png" height="14px"> <?= $item ?></li>
+      <?php endif ?>
+    <?php endforeach; ?>
   </ul>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem iusto explicabo dolor dicta. Quisquam repellat sint sunt laudantium quasi cumque fuga omnis, ducimus nesciunt debitis. Ad est, ullam porro dolorum!</p>
 </li>
