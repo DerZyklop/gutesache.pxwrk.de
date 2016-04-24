e0cf9958d9fcfeba9632d5a7b0f5b07249f54b55
DerZyklop <mail@der-zyklop.de>
2016-04-24 19:11:55 +0200

    miris content

---
+++ site/accounts/miri.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: miri
+firstname: Miriam
+lastname: Gückel
+email: mail@gueckel-design.de
+password: >
+  $2a$10$F9MLcJyzvkgz2mCfqf2wl.z3z3ZsJ1NDBdTmkA0BZ7AES5PnzsD0K
+language: de
+role: admin
+history:
+  - register/miri-eg
