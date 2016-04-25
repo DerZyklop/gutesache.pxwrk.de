6af07262ddef3392a1d736dd618ddbbe663a84c0
DerZyklop <mail@der-zyklop.de>
2016-04-25 19:15:40 +0200

    teaser-hover -> transparent

--- site/snippets/teaser.php
+++ site/snippets/teaser.php
@@ -1,5 +1,11 @@
 <div class="teaser no-link-highlighting">
   <a href="<?= $item->url() ?>">
+    <div class="front">
+      <h4 class="teaser-title"><?= $item->title() ?></h4>
+      <?php $featured_eg = $pages->find("register/".$item->egname()) ?>
+      <h5 class="teaser-eg-title"><?= $featured_eg->title() ?></h5>
+      <h5 class="teaser-tag"><?= $item->tag() ?></h5>
+    </div>
     <div class="back">
       <?php if ($item->hasImages()): ?>
         <?= thumb($item->images()->first(), array(
@@ -14,12 +20,6 @@
           'crop' => true
         )) ?>
       <?php endif ?>
-    </div>
-    <div class="front">
-      <h4 class="teaser-title"><?= $item->title() ?></h4>
-      <?php $featured_eg = $pages->find("register/".$item->egname()) ?>
-      <h5 class="teaser-eg-title"><?= $featured_eg->title() ?></h5>
-      <h5 class="teaser-tag"><?= $item->tag() ?></h5>
     </div>
   </a>
 </div>
