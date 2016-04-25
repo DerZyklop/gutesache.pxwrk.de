b5bd460687091648861b4d770ad59bae09431db7
DerZyklop <mail@der-zyklop.de>
2016-04-25 21:03:27 +0200

    margins and paddings

--- site/snippets/eg-meta.php
+++ site/snippets/eg-meta.php
@@ -1,44 +1,32 @@
-<ul class="flatList cleanList flex">
+<ul class="eg-meta cleanList flex">
   <li>
     <h5>Verband</h5>
-    <ul class="cleanList">
-      <li>
-        <h6>
-          <?php if ($page->verband() == "") : ?>
-            -
-          <?php else : ?>
-            <a href="#"><?= $page->verband() ?></a>
-          <?php endif ?>
-        </h6>
-      </li>
-    </ul>
+    <h6>
+      <?php if ($page->verband() == "") : ?>
+        -
+      <?php else : ?>
+        <a href="#"><?= $page->verband() ?></a>
+      <?php endif ?>
+    </h6>
   </li>
   <li>
     <h5>Kategorie</h5>
-    <ul class="cleanList">
-      <li>
-        <h6>
-          <?php if ($page->kategorie() == "") : ?>
-            -
-          <?php else : ?>
-            <a href="#"><?= $page->kategorie() ?></a>
-          <?php endif ?>
-        </h6>
-      </li>
-    </ul>
+    <h6>
+      <?php if ($page->kategorie() == "") : ?>
+        -
+      <?php else : ?>
+        <a href="#"><?= $page->kategorie() ?></a>
+      <?php endif ?>
+    </h6>
   </li>
   <li>
     <h5>Branche</h5>
-    <ul class="cleanList">
-      <li>
-        <h6>
-          <?php if ($page->branche() == "") : ?>
-            -
-          <?php else : ?>
-            <a href="#"><?= $page->branche() ?></a>
-          <?php endif ?>
-        </h6>
-      </li>
-    </ul>
+    <h6>
+      <?php if ($page->branche() == "") : ?>
+        -
+      <?php else : ?>
+        <a href="#"><?= $page->branche() ?></a>
+      <?php endif ?>
+    </h6>
   </li>
 </ul>
