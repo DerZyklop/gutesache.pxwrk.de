161e553472aff6896178256a5d8e4a62de0bbb73
DerZyklop <mail@der-zyklop.de>
2016-04-25 17:24:49 +0200

    more icons

--- site/snippets/interactive.php
+++ site/snippets/interactive.php
@@ -1,6 +1,6 @@
 <ul class="cleanList flatList flex flex-start">
-  <li><h5 class="on-open-profile"><img src="/assets/images/google-location-icon-icon-location.png" height="12px"> <a href="<?= $company->url() ?>">Profil</a></h5></li>
-  <li><h5 class="on-open-profile"><img src="/assets/images/google-location-icon-icon-location.png" height="12px"> <a href="mailto:<?= $company->mail() ?>">Kontakt</a></h5></li>
+  <li><h5 class="on-open-profile"><a href="<?= $company->url() ?>"><span class="fa fa-users"></span> Profil</a></h5></li>
+  <li><h5 class="on-open-profile"><a href="mailto:<?= $company->mail() ?>">E-Mail <span class="fa fa-envelope-o"></span></a></h5></li>
   <?php
     if (!function_exists("addhttp")) {
       function addhttp($url) {
@@ -11,5 +11,5 @@
       }
     }
   ?>
-  <li><h5 class="on-open-profile"><a href="<?= addhttp($company->website()) ?>">➡ Webseite</a></h5></li>
+  <li><h5 class="on-open-profile"><a href="<?= addhttp($company->website()) ?>">Webseite <span class="fa fa-external-link"></span></a></h5></li>
 </ul>
