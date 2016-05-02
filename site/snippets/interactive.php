5c919599b8b8b5781fc8beba03c82caf3ec9f612
DerZyklop <mail@der-zyklop.de>
2016-05-02 17:00:07 +0200

    fixed interactive

--- site/snippets/interactive.php
+++ site/snippets/interactive.php
@@ -1,6 +1,6 @@
 <ul class="cleanList flatList flex flex-start">
   <li><h5 class="on-open-profile"><a href="<?= $company->url() ?>"><span class="fa fa-users"></span> Profil</a></h5></li>
-  <li><h5 class="on-open-profile"><a href="mailto:<?= $company->mail() ?>">E-Mail <span class="fa fa-envelope-o"></span></a></h5></li>
+  <li><h5><a href="mailto:<?= $company->mail() ?>">E-Mail <span class="fa fa-envelope-o"></span></a></h5></li>
   <?php
     if (!function_exists("addhttp")) {
       function addhttp($url) {
@@ -11,5 +11,6 @@
       }
     }
   ?>
-  <li><h5 class="on-open-profile"><a href="<?= addhttp($company->website()) ?>">Webseite <span class="fa fa-external-link"></span></a></h5></li>
+  <li><h5><a href="<?= addhttp($company->website()) ?>">Webseite <span class="fa fa-external-link"></span></a></h5></li>
+  <!-- <li><h5 class=""><a href="<?= $company->website() ?>">Merken <span class="fa fa-external-link"></span></a></h5></li> -->
 </ul>
